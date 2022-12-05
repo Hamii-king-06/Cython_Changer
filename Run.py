bcf2cf1390bd588420addf76d9799b8fcbd4c41d
HAMID MEER <85035495+Hamii-king-06@users.noreply.github.com>
2022-12-05 17:06:21 +0500

    Update Run.py

--- Run.py
+++ Run.py
@@ -9,13 +9,13 @@ bit = platform.architecture()[0]
 
 if bit == '64bit':
 
-    from arch64 import XYZ
+    from C64 import main
 
-    XYZ()
+    main()
 
 elif bit == '32bit':
 
-    from arch32 import XYZ
+    from C23 import main
 
-    XYZ()
+    main()
 
